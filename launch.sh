08cd68e674ac10bcb2c8bb5352a1e0a80d78f47d
Zygimantass <zygimantelis@gmail.com>
2022-06-15 13:46:30 -0400

    add: add a launch script for figmates

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROTOCOL=$1
+
+RPC_URL=$(curl https://cosmos-chain.directory/chains/$PROTOCOL --silent | jq ".apis.rpc[0].address" -r)
+
+if [[ "$RPC_URL" != *"443"* ]]; then
+  RPC_URL="$RPC_URL:443"
+fi
+
+cd $1
+docker compose up -d
+docker compose exec -it -e NODE=$RPC_URL node_1 /bin/bash
